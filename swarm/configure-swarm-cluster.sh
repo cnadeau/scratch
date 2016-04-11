a1c27068d1228e01317631ca80559a479eba5433
Christian Nadeau <cnadeau@macadamian.com>
2016-04-11 12:54:14 -0400

    Use docker-composee 1.6.2

--- swarm/configure-swarm-cluster.sh
+++ swarm/configure-swarm-cluster.sh
@@ -152,7 +152,7 @@ if isagent ; then
 fi
 
 echo "Installing docker compose"
-curl -L https://github.com/docker/compose/releases/download/1.5.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 sudo service docker restart
